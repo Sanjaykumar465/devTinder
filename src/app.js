1e1284ed1e605c057e1d239b41c21ac17ede40e1
Sanjay <smohanta580@gmail.com>
2025-07-10 11:05:29 +0530

    Creating login API

--- src/app.js
+++ src/app.js
@@ -31,6 +31,25 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) =>{
+   try{
+       const {email, password} = req.body
+     const user = await User.findOne({ email });
+     if(!user) {
+        throw new Error("Invlid Credentials");
+     }
+     const isPasswordValid = await bcrypt.compare(password, user.password)
+     if(isPasswordValid) {
+        res.send("Login Successful");
+     } else {
+      throw new Error("Invalid Credentials");
+     }
+
+   } catch (err) {
+    res.status(400).send("ERROR" + err.message);
+  }
+})
+
 app.get("/user", async (req, res) => {
   const userEmail = req.body.emailld;
 
